f92fb52add0d36e18caef9b7dda6120b19dc2503
stoneand2 <stoneand2@users.noreply.github.com>
2016-03-10 18:51:25 -0600

    Beginning to work on functions
    Beginning to create Candidate class, write function to help user create
    a candidate, write function to calculate delegates needed.

--- Stone_PS6_Development.R
+++ Stone_PS6_Development.R
@@ -8,5 +8,59 @@ setwd("~/github/PS6")
 # Packages to be utilized 
 library(devtools); library(roxygen2)
 
+Candidate <- setClass(Class="Candidate", 
+                      slots = c(
+                        name = "character",
+                        delegatesWon = "numeric",
+                        party = "character", 
+                        delegatesNeeded = "numeric"
+                      ),
+                      prototype = prototype(
+                        name = "",
+                        delegatesWon = c(),
+                        party = "",
+                        delegatesNeeded = c()
+                      )
+)
+
+createCandidate <- setClass(Class="createCandidate")
+
+# romney <- Candidate(name="Romney",delegatesWon=22, party="Republican", delegatesNeeded=1298)
+
+setGeneric(name="createCandidate",
+           def=function(name, delegatesWon, party)
+           {standardGeneric("createCandidate")}
+)
+
+setMethod(f="createCandidate",
+          definition=function(name, delegatesWon, party){
+            return(new("Candidate", name=name, delegatesWon = delegatesWon, 
+                       party = party, delegatesNeeded=neededCalculator(party)))
+          }
+)
+
+
+setGeneric(name="neededCalculator",
+           def=function(party)
+           {standardGeneric("neededCalculator")}
+)
+
+setMethod(f="neededCalculator",
+          definition=function(party){
+            if(party=="R" | party=="Rep" | party=="Rep." | party=="Republican"){
+              return(1237)
+            }
+            if(party=="D" | party=="Dem" | party=="Dem." | party=="Democrat" | party=="Democratic"){
+              return(2383)
+            }
+            else{
+              stop("Please specify the party properly with either the string 'Democratic' or 'Republican'")
+            }
+          }
+)
+
+
+
+
 
 
